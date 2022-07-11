c4011bac97fc49ff7ce58714baa4a9bfb7a31d83
minj6000 <minj6000@gmail.com>
2022-07-11 22:14:36 +0900

    Fisrt Commit

---
+++ Assets/Script/PlayerMoving.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+    public float RotationSpeed = 50f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public Vector3 point;
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        //Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(point, -Vector3.up, RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.RotateAround(point, Vector3.up, RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            transform.RotateAround(point, Vector3.right, RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            transform.RotateAround(point, -Vector2.right, RotationSpeed * Time.deltaTime);
+        }
+
+        //if (direction.magnitude >= 0.1f)
+        //{
+        //    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        //    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //    controller.Move(direction * speed * Time.deltaTime);
+        //}
+    }
+}
---
+++ Assets/Script/StartGameCube.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StartGameCube : MonoBehaviour
+{
+    public GameObject StartCube;
+
+    public GameObject front;
+    public GameObject back;
+    public GameObject right;
+    public GameObject left;
+    public GameObject up;
+    public GameObject down;
+
+    //public GameObject[] faces;
+    //public Material Mats;
+    public float a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Mats = new Material[faces.Length];
+
+        //for (float i = 0; i < faces.Length; i++)
+        //{
+        //    Mats[i] = faces[i].GetComponent<Renderer>.material;
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            //for (float i = 0; i < faces.Length; i++)
+            //{
+            //    Fade();
+            //    //faces[i].
+            //}
+
+            //Mats.DoFade(0f, 3f);
+            Fade(up);
+            Fade(down);
+            Fade(left);
+            Fade(right);
+            Fade(front);
+            Fade(back);
+            
+            
+            //Starting(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            up.SetActive(false);
+            down.SetActive(false);
+            front.SetActive(false);
+            back.SetActive(false);
+            right.SetActive(false);
+            left.SetActive(false);
+        }
+    }
+
+    void Fade(GameObject gameObject)
+    {
+        gameObject.GetComponent<MeshRenderer>().material.DOFade(0.0f, 3.0f);
+        a++;
+    }
+
+    IEnumerator Starting(GameObject gameObject, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        gameObject.SetActive(false);
+    }
+}
